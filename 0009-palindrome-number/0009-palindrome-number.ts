b66f2b8ff5da7e91cb7342810fc9b3d74bd6d865
Pratik Khadtale <pratik.k048@gmail.com>
2023-01-15 14:42:01 +0530

    Time: 177 ms (85.39%), Space: 51 MB (94.20%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -0,0 +1,15 @@
+function isPalindrome(x: number): boolean {
+
+    let reverse =0;
+    let copy =x;
+
+    while (copy>0){
+        let digit =copy%10
+        reverse = reverse *10 +digit
+        copy= Math.floor(copy/10.0)
+    }
+
+    return reverse===x;
+
+
+};
